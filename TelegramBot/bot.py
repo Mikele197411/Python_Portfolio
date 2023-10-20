afb360ac29907da8aab67e687385a542ed5a6eff
Шилков Михаил <mikhailshilkov@gmail.com>
2023-10-20 12:41:58 +0300

    SMALL FIXES

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext, MessageHandler, filters
 import logging
 
 
@@ -8,11 +8,15 @@ import logging
 async def main(update: Update, context: ContextTypes.DEFAULT_TYPE)->None:
     await update.message.reply_text(f"""Hello {update.effective_user.first_name}
     I have only /start command """)
+    
   
-
+async def chat(update: Update, context: CallbackContext):
+    text="Hello guy"
+    await update.message.reply_text(text)
 
 app=ApplicationBuilder().token('6603128229:AAGy0qwJOOpMYSG2EBL7sgkFR5XKU6QEWX0').build()
 app.add_handler(CommandHandler("start", main))
+app.add_handler(MessageHandler(filters.TEXT, chat, ContextTypes.DEFAULT_TYPE))
 #ogging.basicConfig(filename='bot.log', filemode='w', format='%(name)s=%(levelname)s-%(message)s')
 #logging.info('Bot started')
 app.run_polling()

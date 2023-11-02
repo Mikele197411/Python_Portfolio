a657bd5ce85e374aca77f4736ad56d80399d05ec
Шилков Михаил <mikhailshilkov@gmail.com>
2023-11-02 09:35:46 +0200

    added audio handling

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -14,9 +14,13 @@ async def chat(update: Update, context: CallbackContext):
     text="Hello guy"
     await update.message.reply_text(text)
 
+async def get_voice(update: Update, context: ContextTypes.DEFAULT_TYPE)->None:
+    new_file=await context.bot.get_file(update.message.voice.file_id)
+    await new_file.download_to_drive(f"voice_note.ogg")
+
 app=ApplicationBuilder().token('6603128229:AAGy0qwJOOpMYSG2EBL7sgkFR5XKU6QEWX0').build()
 app.add_handler(CommandHandler("start", main))
 app.add_handler(MessageHandler(filters.TEXT, chat, ContextTypes.DEFAULT_TYPE))
-#ogging.basicConfig(filename='bot.log', filemode='w', format='%(name)s=%(levelname)s-%(message)s')
-#logging.info('Bot started')
+app.add_handler(MessageHandler(filters.VOICE, get_voice, ContextTypes.DEFAULT_TYPE))
+
 app.run_polling()
